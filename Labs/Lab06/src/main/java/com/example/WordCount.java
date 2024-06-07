3fe7e36c7d626352a6b6e455b4fe6a791dabc63d
r1anl3 <levu1409@gmail.com>
2024-06-07 19:37:15 +0700

    run code in local

--- Labs/Lab06/src/main/java/com/example/WordCount.java
+++ Labs/Lab06/src/main/java/com/example/WordCount.java
@@ -16,22 +16,22 @@ import scala.Tuple2;
 public class WordCount {
     public static void main(String[] args) throws IOException {
         // Check inputs
-        if (args.length < 2) {
-            System.err.println("Missing input/output path");
-            System.exit(1);
-        }
+//        if (args.length < 2) {
+//            System.err.println("Missing input/output path");
+//            System.exit(1);
+//        }
         // Parse inputs
-        String inputPath = args[0];
-        String outputPath = args[1];
-        // String inputPath = "file:///D://WorkSpace/Java-project/lab06-hadoop/input.txt";
-        // String outputPath = "file:///D://WorkSpace/Java-project/lab06-hadoop/output.txt";
+//        String inputPath = args[0];
+//        String outputPath = args[1];
+         String inputPath = "input.txt";
+         String outputPath = "output.txt";
         // Init sparkConfig and SparkContext
         // Un-comment this line for debugging locally
-        // SparkConf sparkConf = new SparkConf().setAppName("EvenNumbersCount").setMaster("local[2]").set("spark.executor.memory","1g");
+         SparkConf sparkConf = new SparkConf().setAppName("EvenNumbersCount").setMaster("local[2]").set("spark.executor.memory","1g");
         // Comment this line for debugging locally
-        SparkConf sparkConf = new SparkConf().setAppName("WordCount");
-        sparkConf.set("spark.driver.extraJavaOptions", "-XX:+UseG1GC");
-        sparkConf.set("spark.executor.extraJavaOptions", "-XX:+UseG1GC");
+//        SparkConf sparkConf = new SparkConf().setAppName("WordCount");
+//        sparkConf.set("spark.driver.extraJavaOptions", "-XX:+UseG1GC");
+//        sparkConf.set("spark.executor.extraJavaOptions", "-XX:+UseG1GC");
         JavaSparkContext sparkContext = new JavaSparkContext(sparkConf);
         JavaRDD<String> textFile = sparkContext.textFile(inputPath).cache();
         // Load data from file, distribute and process data
